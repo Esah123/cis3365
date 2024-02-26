7ceaf9fce9062d9becbb2a99bd9fc28cae1af731
Esah123 <130365590+Esah123@users.noreply.github.com>
2024-02-26 02:51:23 -0600

    Delete sprint1 directory

--- sprint1/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Basketball23!'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Basketball23!@cis3368.cfkaqeuq6e3f.us-east-1.rds.amazonaws.com/cis3368'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'BasketBall23!'
