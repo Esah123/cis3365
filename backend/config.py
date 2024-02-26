5885f3b59972c25fa2a5cd10ac4a5c676d34ecf7
esah123 <esahb1234@gmail.com>
2024-02-26 03:01:48 -0600

    Sprint1

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Basketball23!'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Basketball23!@cis3368.cfkaqeuq6e3f.us-east-1.rds.amazonaws.com/cis3368'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'BasketBall23!'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'Basketball23!'
