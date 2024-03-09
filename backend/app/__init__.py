61f76bcc9c762679b37021d0360973a4acc870a0
Esah123 <130365590+Esah123@users.noreply.github.com>
2024-03-08 21:40:11 -0600

    Initializing Application

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_jwt_extended import JWTManager
@@ -6,7 +7,11 @@ from flask_jwt_extended import JWTManager
 db = SQLAlchemy()
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__,
+                static_url_path='', 
+                static_folder='../../frontend',
+                template_folder='../../frontend')
+    CORS(app)
     app.config.from_object(config_class)
 
     db.init_app(app)
